a2b2f48e17f9251dfb43ab3549be1425ef20c051
Chenx221 <jiaxinghuoyan@163.com>
2020-02-21 09:28:03 +0800

    Update core.mk

--- build/target/product/core.mk
+++ build/target/product/core.mk
@@ -23,12 +23,6 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.alarm_alert=Alarm_Classic.ogg
 
 PRODUCT_PACKAGES := \
-    bcm_dut
-    audio.a2dp.default \
-    libkeyutils \
-    libjpega \ 
-    libexifa \
-    sap \
     ApplicationsProvider \
     BackupRestoreConfirmation \
     Browser \
